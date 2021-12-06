282948dc97de66c85e616b97bff7764d45d72cdf
Ladislav Ondris <ladislav.ondris@seznam.cz>
2021-12-06 22:31:49 +0100

    Calling copy on the instance of Map

--- dicewars/ai/ladatron/brs.py
+++ dicewars/ai/ladatron/brs.py
@@ -23,7 +23,7 @@ class BestReplySearch:
         best_move: Move = moves[0]
 
         for move in moves:
-            map_copy = copy.deepcopy(map)
+            map_copy = map.copy()
             move.do(map_copy)
             value = self._search(player, opponents, map, depth=3, turn=Turn.MIN, alpha=-inf, beta=inf)
 
@@ -50,7 +50,7 @@ class BestReplySearch:
         for move in moves:
             # Each move will be simulated in its own map.
             # This is to avoid move undoing if there is only a single map instance.
-            map_copy = copy.deepcopy(map)
+            map_copy = map.copy()
             move.do(map_copy)
             value = -self._search(player, opponents, map_copy, depth - 1, turn, -beta, -alpha)
 
